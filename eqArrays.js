d38108184193e0b8146bbf7b64cda5fd57f52f70
Simon Jung <simon.hb.jung@hotmail.com>
2020-06-23 21:57:11 +0000

    equal arrays

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} ==== ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} ==== ${expected}`);
+  }
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
