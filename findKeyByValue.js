04332b5b5f26c77dc1f6bd5dc55e404772ceb5eb
Simon Jung <simon.hb.jung@hotmail.com>
2020-06-24 23:22:58 +0000

    findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+const findKeyByValue = function(inputObject, searchValue) {
+  for (let key in inputObject) {
+    if (inputObject[key] === searchValue) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const bootcampFriends = {
+  neighbour: "Julianna",
+  chessBud: "Kaushik",
+  researchPartner:  "Will"
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bootcampFriends, "Julianna"), "neighbour");
+assertEqual(findKeyByValue(bootcampFriends, "Moses"), undefined);
