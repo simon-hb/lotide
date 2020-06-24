a8de44fa31fad7bfa2ea1f7e2d9ac68036c78000
Simon Jung <simon.hb.jung@hotmail.com>
2020-06-24 18:29:36 +0000

    fixed 1 thing

--- middle.js
+++ middle.js
@@ -36,7 +36,7 @@ const middle = function (inputArray) {
   let middleArray = [];
   if (inputArray.length < 3) {
     return middleArray;
-  } else if (isEven(inputArray.length) === true) {
+  } else if (isEven(inputArray.length)) {
     middleArray.push(inputArray[inputArray.length / 2 - 1]);
     middleArray.push(inputArray[inputArray.length / 2]);
   } else {
