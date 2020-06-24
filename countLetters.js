eead6d2e776aedf6bacedb33647b05d5c40fbbdd
Simon Jung <simon.hb.jung@hotmail.com>
2020-06-24 22:16:01 +0000

    countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+const countLetters = function (stringInput) {
+  results = {};
+  for (let input of stringInput) {
+    if (input !== ' ') {
+      if (results[input]) {
+        results[input] ++
+      } else
+      results[input] = 1
+    }
+  }
+  return results;
+};
+
+console.log(countLetters('hi my name is simon'));
+console.log(assertEqual(countLetters('hahaha')['h'], 3));
