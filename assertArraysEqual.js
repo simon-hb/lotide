64b2d3c2e246d84d4bbeaced2e745b3052985587
Simon Jung <simon.hb.jung@hotmail.com>
2020-06-24 17:12:40 +0000

    corrected number of equal signs in message

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,16 @@
 const assertArraysEqual = function(arrayOne, arrayTwo) {
   if (Array.isArray(arrayOne) === false || Array.isArray(arrayTwo) === false) {
-    return `🛑🛑🛑Assertion Failed: ${arrayOne} ==== ${arrayTwo}`;
+    return `🛑🛑🛑Assertion Failed: ${arrayOne} === ${arrayTwo}`;
   }
   if (arrayOne.length !== arrayTwo.length) {
-    return `🛑🛑🛑Assertion Failed: ${arrayOne} ==== ${arrayTwo}`;
+    return `🛑🛑🛑Assertion Failed: ${arrayOne} === ${arrayTwo}`;
   }
   for (let i = 0; i < arrayOne.length; i++) {
     if (arrayOne[i] !== arrayTwo[i]) {
-      return `🛑🛑🛑Assertion Failed: ${arrayOne} ==== ${arrayTwo}`;
+      return `🛑🛑🛑Assertion Failed: ${arrayOne} === ${arrayTwo}`;
     }
   }
-  return `✅✅✅Assertion Passed: ${arrayOne} ==== ${arrayTwo}`;
+  return `✅✅✅Assertion Passed: ${arrayOne} === ${arrayTwo}`;
 };
 
 console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
