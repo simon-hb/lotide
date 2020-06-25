4803313500c798e4b085544664d0f3b9da1c20a9
Simon Jung <simon.hb.jung@hotmail.com>
2020-06-25 01:31:33 +0000

    assertObjectsEqual.js

---
+++ assertObjectsEqual.js
@@ -0,0 +1,66 @@
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${inspect(actual)} === ${inspect(expected)}`);
+  }
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (let key in object1) {
+    if (Array.isArray(object1[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (Array.isArray(arrayOne) === false || Array.isArray(arrayTwo) === false) {
+    return false;
+  }
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+eqObjects(ab, ba); // => true
+assertObjectsEqual(ab, ba);
+
+const abc = { a: "1", b: "2", c: "3" };
+eqObjects(ab, abc); // => false
+assertObjectsEqual(ab, abc);
+
+const abd = { a: "2", b: "1" };
+assertObjectsEqual(ab, abd); // false
+
+const bcd = { a: "1", b: 2 };
+assertObjectsEqual(ab, bcd); //false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+eqObjects(cd, dc); // => true
+assertObjectsEqual(cd, dc); 
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+eqObjects(cd, cd2); // => false
+assertObjectsEqual(cd, cd2); 
+
+const cd3 = { d: ["2", 3], c: 1 };
+assertObjectsEqual(cd, cd3); //false
