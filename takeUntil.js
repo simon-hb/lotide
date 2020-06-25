6beaf07b7b5e0aaae6ee8568478343c91a0e922c
Simon Jung <simon.hb.jung@hotmail.com>
2020-06-25 23:29:49 +0000

    takeUntil.js slice  until truthy value is returned

---
+++ takeUntil.js
@@ -0,0 +1,58 @@
+const takeUntil = function(array, callback) {
+  let result = [];
+  for (let element of array) {
+    if (!callback(element)) {
+      result.push(element);
+    } else{
+      return result;
+    }
+  }
+};
+
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+/* expected output
+[ 1, 2, 5, 7, 2 ]
+---
+[ 'I\'ve', 'been', 'to', 'Hollywood' ]
+*/
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (Array.isArray(arrayOne) === false || Array.isArray(arrayTwo) === false) {
+    return false;
+  }
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const dataThree = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const resultsThree = takeUntil(dataThree, x => x < 100);
+assertArraysEqual(resultsThree, []);
+
+const dataFour = ["doe", "ray", "me", "far", "sew", "la", "tea", "doe", "a deer"];
+const resultsFour = takeUntil(dataFour, x => x.length < 3);
+assertArraysEqual(resultsFour, ['doe', 'ray']);
