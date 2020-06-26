96492cf913373062cdee75753fa7fdd5ce35c606
Simon Jung <simon.hb.jung@hotmail.com>
2020-06-26 00:06:34 +0000

    fineKey.js finds key from an object using callback function

---
+++ findKey.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+console.log(
+  findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)
+);
+
+assertEqual(
+findKey({
+    sci_fi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire"
+  }, x => x.includes('Wire')), 'drama');
+
+  const bootcampFriends = {
+    neighbour: "Julianna",
+    chessBud: "Kaushik",
+    researchPartner:  "Will",
+    teacher: 'Janelle',
+    sameInterview: 'Natalie'
+  };
+
+  assertEqual(findKey(bootcampFriends, x => x[0] === 'J'), 'neighbour');
