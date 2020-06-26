5cb1e02129ad9ac9cc58270d5c556901f81906d6
Simon Jung <simon.hb.jung@hotmail.com>
2020-06-26 00:53:08 +0000

    takeUntil.js defactored

--- takeUntil.js
+++ takeUntil.js
@@ -1,11 +1,10 @@
 const takeUntil = function(array, callback) {
   let result = [];
   for (let element of array) {
-    if (!callback(element)) {
-      result.push(element);
-    } else{
+    if (callback(element)) {
       return result;
     }
+    result.push(element);
   }
 };
 
