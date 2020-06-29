c5bd0fde378a86a26186f3367bca1a9a69064cb1
Simon Jung <simon.hb.jung@hotmail.com>
2020-06-29 19:30:56 +0000

    deleted extra comments in tail test (old test code)

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,19 +1,3 @@
-// const assertEqual = require('../assertEqual');
-// const tail = require('../tail');
-
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result.length, 2);
-// assertEqual(result[0], "Lighthouse");
-// assertEqual(result[1], "Labs");
-
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words);
-// assertEqual(words.length, 3);
-
-// console.log(tail(['hi']));
-// console.log(tail([]));
-// console.log(tail([1, 2, 3, 4]));
-
 const assert = require('chai').assert;
 const tail   = require('../tail');
 
