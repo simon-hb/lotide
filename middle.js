07b88e4c4318b538950fb9f46871fa8288d00ec9
Simon Jung <simon.hb.jung@hotmail.com>
2020-06-29 18:06:02 +0000

    test module.export require for middle.js

--- middle.js
+++ middle.js
@@ -1,30 +1,3 @@
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} === ${expected}`);
-  }
-};
-
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (Array.isArray(arrayOne) === false || Array.isArray(arrayTwo) === false) {
-    return false;
-  }
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (Array.isArray(arrayOne[i])) {
-      if (!eqArrays(arrayOne[i], arrayTwo[i])) {
-        return false;
-      }
-    } else if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
 const isEven  = function(num) {
   return num % 2 === 0;
 };
@@ -42,8 +15,4 @@ const middle = function (inputArray) {
   return middleArray;
 };
 
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle(['a', 'b', 'c', 'd', 'e']), ['c']);
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,8 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+assertArraysEqual(middle([]), []);
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle(['a', 'b', 'c', 'd', 'e']), ['c']);
